6797396d05bf128cd3883496820efe14894c7cf7
knowledgebao <knowledgebao@163.com>
2019-05-06 10:59:45 +0800

    test

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	//"time"
+)
+
+//Lazy Evaluation
+func Interge() <-chan int {
+	yield := make(chan int, 12)
+	count := 0
+	go func() {
+		for {
+			yield <- count
+			fmt.Println("gorount:" + strconv.Itoa(count))
+			count++
+		}
+	}()
+	return yield
+}
+
+var resume <-chan int
+
+func getInetge() int {
+	return <-resume
+}
+
+func main() {
+	resume = Interge()
+	//	time.Sleep(time.Second * 5)
+	fmt.Println(getInetge())
+	//time.Sleep(time.Second * 5)
+	fmt.Println(getInetge())
+	fmt.Println(getInetge())
+	fmt.Println(getInetge())
+	fmt.Println(getInetge())
+	fmt.Println(getInetge())
+}
